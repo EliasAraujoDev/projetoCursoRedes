f435cdbef87b99f127c8e607606022c6edb20a51
renaneliakim1 <140713357+renaneliakim1@users.noreply.github.com>
2024-01-05 08:47:18 -0300

    melhorias

--- views/atendimento.php
+++ views/atendimento.php
@@ -18,7 +18,7 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/projetocursoredes/templates/cabecalho
             <div>
                 <label for="cpf">Digite o CPF do cliente:</label>
                 <input type="text" id="cpf" placeholder="Digite o CPF">
-                <button onclick="searchByCPF()">Buscar por CPF</button>
+                <button type="button" class="btn btn-primary" onclick="searchByCPF()">Buscar por CPF</button>
             </div>
             <p>Nome: João da Silva</p>
             <p>CPF: 123.456.789-00</p>
@@ -31,30 +31,42 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/projetocursoredes/templates/cabecalho
                 <label class="form-check-label" for="flexRadioDefault1">
                     Adquirir produto
                 </label>
+
+<!--                 <label for="service-type">Tipo de Serviço:</label> -->
+                <select id="service-type">
+                    <option value="internet">Internet</option>
+                    <option value="drone">Drone</option>
+                    <option value="cameras">Câmeras</option>
+                    <option value="fiber-repair">Reparo de Fibra Óptica</option>
+                </select>
+
+                <br>
+
             </div>
             <div class="form-check">
                 <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
                 <label class="form-check-label" for="flexRadioDefault2">
                     Reparo de produto
                 </label>
-            </div>
+            
 
-
-            <label for="service-type">Tipo de Serviço:</label>
+<!--             <label for="service-type">Tipo de Serviço:</label> -->
             <select id="service-type">
                 <option value="internet">Internet</option>
                 <option value="drone">Drone</option>
                 <option value="cameras">Câmeras</option>
                 <option value="fiber-repair">Reparo de Fibra Óptica</option>
             </select>
+
+            </div>
         </div>
 
 
 
         <div class="options">
-            <button class="end-call" onclick="endCall()">Finalizar Atendimento</button>
-            <button class="generate-protocol" onclick="generateProtocol()">Gerar Protocolo</button>
-            <button class="generate-equipe" onclick="generateEquipe()">Equipe Técnica</button>
+            <button type="button" class="btn btn-primary" class="end-call" onclick="endCall()">Finalizar Atendimento</button>
+            <button type="button" class="btn btn-primary" class="generate-protocol" onclick="generateProtocol()">Consultar Protocolo</button>
+            <button type="button" class="btn btn-primary" class="generate-equipe" onclick="generateEquipe()">Equipe Técnica</button>
 
         </div>
     </div>
@@ -81,7 +93,7 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/projetocursoredes/templates/cabecalho
     }
 
     function generateProtocol() {
-        alert('Protocolo gerado.');
+        alert('Protocolo gerado: 025863549525.');
     }
 </script>
 
