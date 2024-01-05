acb959cd485e2629f9a6c6e002ff3d28643e5698
renaneliakim1 <renaneliakim1@gmail.com>
2024-01-05 14:32:57 -0300

    pequenas alteraçoes

--- db/conexao.php
+++ db/conexao.php
@@ -1,5 +1,5 @@
 <?php
-require_once $_SERVER["DOCUMENT_ROOT"] . "/projeto/configs/config.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/projetocursoredes/configs/config.php";
 
 $servername = DBLOCAL;
 $username = USUARIO;
