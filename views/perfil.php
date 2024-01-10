4f78bdceb8e61c3e213b644810d6822d646f6405
renaneliakim1 <140713357+renaneliakim1@users.noreply.github.com>
2024-01-09 21:20:06 -0300

    diversas melhorias

--- views/perfil.php
+++ views/perfil.php
@@ -8,12 +8,14 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/projetocursoredes/templates/cabecalho
 
 <div id="perfil">
 
-   
-    <div class="container text-center">
 
+    <div class="container text-center">
         <h4>Bem vindo, João Silva!</h4>
 
-        <h3>Configuração de seus serviços</h3>
+        <br>
+        <div id="titulo" >
+            <h3>Configuração de seus serviços</h3>
+        </div>
         <div class="row">
             <div class="col">
                 <p><b>CÂMERAS</p></b>
